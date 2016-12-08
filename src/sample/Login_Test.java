5098570c150fb04bce63649ca6b80da494d2ef33
isaak78 <isaak.ccn@gmail.com>
2016-12-08 10:17:08 +0000

    Add files via upload

--- src/sample/Login_Test.java
+++ src/sample/Login_Test.java
@@ -15,15 +15,18 @@ public class Login_Test extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("Valid CC Check: " + UtilsForm.luhnCheck("112652620ZZ9")); // TODO done :)
-        System.out.println("REGEX CC Check: " + UtilsForm.isCcValid("112650620Yz3"));
-        System.out.println("apache.commons.EmailValidator: isvalid@gmail.com  -> " + UtilsForm.isEmaiValid("isvalid@gmail.com"));
-        System.out.println("apache.commons.EmailValidator: is.valid.gmail.com  -> " + UtilsForm.isEmaiValid("is.valid.gmail.com"));
+        System.out.println("Valid CC Check: " + UtilsForm.luhnCheck("112485620ZZ3"));
+        System.out.println("REGEX CC Check: " + UtilsForm.isCcValid("112350620Yz3"));
+        System.out.println("apache.commons.EmailValidator: isthis.one@gmail.com  -> " + UtilsForm.isEmaiValid("isthis.one@gmail.com"));
+        System.out.println("apache.commons.EmailValidator: isNot.valid.gmail.com  -> " + UtilsForm.isEmaiValid("isNot.valid.gmail.com"));
         System.out.println("apache.commons.EmailValidator: i@g.c                -> " + UtilsForm.isEmaiValid("i@g.c"));
         System.out.println(UtilsForm.isValidNif(123456780));
         System.out.println(UtilsForm.isValidNif(1));
         SecurityKey.securiryTest("sherLOCKEDhomes");
+        System.out.println("PassWord TXT = sherLOCKEDhomes");
+        System.out.println("Pop-uP valid password test");
 
+        UtilsForm.validaPassword("sherrd");
 
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml")); //FXMLDocument pag de login
         Scene scene_login = new Scene(root);
@@ -35,5 +38,5 @@ public class Login_Test extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+    //Todo finish block status user on DB
 }
