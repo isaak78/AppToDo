73c9c992d08c2ae77670c8ccb1adfc48ef94b78c
isaak78 <isaak.ccn@gmail.com>
2016-12-13 00:32:25 +0000

    Add files via upload

--- src/sample/SecurityKey.java
+++ src/sample/SecurityKey.java
@@ -6,6 +6,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by pc on 12/6/16.
+ *
+ * EX: $2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa
+ *
+ * 2a identifies the bcrypt algorithm version that was used.
+ * 10 is the cost factor; 210 iterations of the key derivation function are used
+ * vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa
+ * is the salt and the cipher text, concatenated and encoded in a modified Base-64.
+ * The first 22 characters decode to a 16-byte value for the salt.
+ * The remaining characters are cipher text to be compared for authentication.
+ *
+ * password #=> "secret1"
  */
 
 
